881c43bbbd7f02791f8462ecdcd6e099608c38e4
hasangazikarasahin <hasangazikarasahin@gmail.com>
2022-02-09 17:50:11 +0300

    static files

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const fn = function () {
-  console.log('Hasan');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const fnLog = (req, res, next) => {
+  console.log('Log atildi.');
+  next();
 };
 
-fn();
+app.use(express.static('public'));
+app.use(fnLog);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
